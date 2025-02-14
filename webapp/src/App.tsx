48e414227c775aa95141e38addef0224dbf82309
crossfire3001 <obiwan2035@mail.ru>
2025-02-14 16:28:55 +0500

    JS

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -1,13 +1,22 @@
 export const App = () => {
+  const ideas = [
+    { idea: "cool idea 1", name: "idea-1", desc: "cool idea desc 1" },
+    { idea: "cool idea 2", name: "idea-2", desc: "cool idea desc 2" },
+    { idea: "cool idea 3", name: "idea-3", desc: "cool idea desc 3" },
+    { idea: "cool idea 4", name: "idea-4", desc: "cool idea desc 4" },
+    { idea: "cool idea 5", name: "idea-5", desc: "cool idea desc 5" },
+  ];
   return (
     <div>
-      <h1>IdeaNick</h1>
-      <div>
-        <div>
-          <h2>Idea 1</h2>
-          <p>Idea 1 desc</p>
-        </div>
-      </div>
+      <h1>Ideas</h1>
+      {ideas.map((idea) => {
+        return (
+          <div key={idea.idea}>
+            <h2>{idea.name}</h2>
+            <p>{idea.desc}</p>
+          </div>
+        );
+      })}
     </div>
   );
 };
